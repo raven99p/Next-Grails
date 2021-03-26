a8a1440db00cc5a14fe6940e6b406b87b30df213
pellul <paulinhoellul@gmail.com>
2021-03-26 14:11:53 +0200

    Fixed #7

--- utilitieFunctions/departmentFetchingFunctions.js
+++ utilitieFunctions/departmentFetchingFunctions.js
@@ -20,44 +20,45 @@ export async function createDepartment(props){
 export async function updateDepartment(props){
     console.log('Creating department..')
     try{
-      const res = await fetch(`http://localhost:8080/departmentResponder/verify.json`, {
-        method: 'POST',
+      const res = await fetch(`http://localhost:8080/departmentResponder/updateDepartment.json`, {
+        method: 'PUT',
         headers:{
           'Accept':'application/json',
           'Content-type':'application/json'
         },
         body: JSON.stringify({
-          username:props.username,
-          password:props.password
+          departmentId: props.departmentId,
+          departmentName: props.departmentName
         })
       });
       return res;
     }catch(e){
       console.log(e)
     }
-  }
+}
 
 
 export async function deleteDepartment(props){
     console.log('Creating department..')
     try{
-      const res = await fetch(`http://localhost:8080/departmentResponder/verify.json`, {
-        method: 'POST',
+      const res = await fetch(`http://localhost:8080/departmentResponder/deleteDepartment.json`, {
+        method: 'DELETE',
         headers:{
           'Accept':'application/json',
           'Content-type':'application/json'
         },
         body: JSON.stringify({
-          username:props.username,
-          password:props.password
+            departmentId: props.departmentId
         })
       });
       return res;
     }catch(e){
       console.log(e)
     }
-  }
+}
+
 /*
         post "/departmentResponder/postDepartment(.$format)"(controller: 'departmentResponder', action:'postDepartment')
         put "/departmentResponder/updateDepartment(.$format)"(controller: 'departmentResponder', action: 'updateDepartment')
-        delete "/departmentResponder/deleteDepartment(.$format)"(controller: 'departmentResponder', action: 'deleteDepartment')*/
+        delete "/departmentResponder/deleteDepartment(.$format)"(controller: 'departmentResponder', action: 'deleteDepartment')
+*/
