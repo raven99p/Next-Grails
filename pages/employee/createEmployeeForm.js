4ef30ff4c12072d063dda2fc0169c07c42f77cd5
pellul <paulinhoellul@gmail.com>
2021-03-28 16:26:26 +0300

    Fixed #16 # 19

--- pages/employee/createEmployeeForm.js
+++ pages/employee/createEmployeeForm.js
@@ -6,19 +6,54 @@ import {
     DatePicker,
     Row,Col
   } from 'antd';
-  import {useRouter} from 'next/router';
-  import LayoutCustom from '../../components/layout'
+import {useRouter} from 'next/router';
+import LayoutCustom from '../../components/layout'
+import { createEmployee } from '../../utilitieFunctions/employeeFetchingFunctions'
 
 
+const { Option } = Select;
 
+export async function getServerSideProps() {
+    console.log('fetching..');
+    try {
+      const res = await fetch(`http://localhost:8080/departmentResponder/getDepartments.json`, {method: 'GET'});
+      const data = await res.json();
+      if(!data)
+      return {
+        notFound: true,
+      }
+      return {
+        props:{
+          data,
+        },
+      }}catch(e) {
+          console.log(e);
+          const error = true
+          return {
+            props:{
+              error,
+            },
+          }
+        }  
+} 
   
-  
-  export default function Hire(props) {
+  export default function createEmployeeForm(props) {
     const router = useRouter();
-      
-      
+    const data = props.data.responseMessage;
+
+    async function handleCreateEmployee(values) {
+      values.dob = values.dob._d;
+      const grailsResponse = await createEmployee(values);
+      const data = await grailsResponse.json();
+      if (data.status==200) {
+        router.push('/employee/showEmployees/'+values.departmentId);
+      }
+    }
+    
+
     return (
-      <LayoutCustom>    
+      <LayoutCustom>   
+        
         <Row>
           <Col span={8}></Col>
           <Col span={8}><br/></Col>
@@ -27,31 +62,35 @@ import {
         <Row>
           <Col span={6}></Col>
           <Col span={12}>
-            <Form labelCol={{ span: 6 }}
+            <Form labelCol={{ span: 8 }}
                   wrapperCol={{ span: 14 }}
                   layout="horizontal"
+                  onFinish={handleCreateEmployee}
             >
-              <Form.Item label="Όνομα">
+              <Form.Item name="firstName" label="Όνομα" type="text" rules={[{ required: true }]} >
+                <Input  />
+              </Form.Item>
+              <Form.Item name="lastName" label="Επώνυμο" type="text" rules={[{ required: true }]}>
                 <Input />
               </Form.Item>
-              <Form.Item label="Επώνυμο">
+              <Form.Item name="afm" label="ΑΦΜ" rules={[{ required: true }]}>
                 <Input />
               </Form.Item>
-              <Form.Item label="ΑΦΜ">
-                <Input />
-              </Form.Item>
-              
-              <Form.Item label="Ημερομηνία γέννησης">
+              <Form.Item name="dob" label="Ημερομηνία γέννησης" rules={[{ required: true }]}>
                 <DatePicker />
               </Form.Item>
-              <Form.Item label="Τμήμα">
-                <Input />
+              <Form.Item name="departmentId" label="Τμήμα"  rules={[{ required: true }]}>
+                <Select>
+                  {data.map((value) => (
+                    <Option value={value.departmentid}>{value.departmentname}</Option>
+                  ))}
+                </Select>
               </Form.Item>
-              <Form.Item>
-                <Button size="large" type="primary">
-                  Hire
+              <Form.Item style={{ textAlign: 'center' }} >
+                <Button type="primary" htmlType="submit" style={{ textAlign: 'center' }}>
+                    Δημιουργία
                 </Button>
-              </Form.Item>
+              </Form.Item> 
             </Form>
 
         </Col>
